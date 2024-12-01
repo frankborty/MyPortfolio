82217455c1e2062c9d3a57ec34435b221d59bed2
Frankborty <dessena.franco@gmail.com>
2024-12-01 09:44:40 +0100

    CleanProject

--- MyPortfolio/Program.cs
+++ MyPortfolio/Program.cs
@@ -1,22 +1,15 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using MyPortfolio.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-builder.Services.AddDbContext<UserDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("UserDBConnection")));
-
+/*
 builder.Services.AddDbContext<DataDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DataDBConnection")));
-
-
-//identity core
-builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<UserDbContext>();
-
+*/
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
